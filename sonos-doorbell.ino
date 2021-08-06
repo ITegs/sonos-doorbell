15fb8b599baf416f3436ddc7e9f99a0473f6895d
Johannes <johannes-pahle@t-online.de>
2021-08-06 17:18:08 +0200

    play()

--- sonos-doorbell.ino
+++ sonos-doorbell.ino
@@ -6,7 +6,7 @@ const char *ssid = ssid_private;         //"YOUR SSID HERE"
 const char *password = password_private; //"YOUR PASSWORD HERE"
 
 char *addtoQueue_Body = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:AddURIToQueue xmlns:u=\"urn:schemas-upnp-org:service:AVTransport:1\"><InstanceID>0</InstanceID>\n<EnqueuedURI>https://bigsoundbank.com/UPLOAD/mp3/2365.mp3</EnqueuedURI><EnqueuedURIMetaData></EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued></DesiredFirstTrackNumberEnqueued><EnqueueAsNext>true</EnqueueAsNext><FirstTrackNumberEnqueued></FirstTrackNumberEnqueued><NumTracksAdded></NumTracksAdded><NewQueueLength></NewQueueLength></u:AddURIToQueue></s:Body></s:Envelope>";
-char *Play_Body = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:AddURIToQueue xmlns:u=\"urn:schemas-upnp-org:service:AVTransport:1\"><InstanceID>0</InstanceID>\n<EnqueuedURI>https://bigsoundbank.com/UPLOAD/mp3/2365.mp3</EnqueuedURI><EnqueuedURIMetaData></EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued></DesiredFirstTrackNumberEnqueued><EnqueueAsNext>true</EnqueueAsNext><FirstTrackNumberEnqueued></FirstTrackNumberEnqueued><NumTracksAdded></NumTracksAdded><NewQueueLength></NewQueueLength></u:AddURIToQueue></s:Body></s:Envelope>";
+char *Play_Body = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:Play xmlns:u=\"urn:schemas-upnp-org:service:AVTransport:1\"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play></s:Body></s:Envelope>";
 
 void initWiFi()
 {
@@ -29,39 +29,41 @@ void setup()
 
 void loop()
 {
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        HTTPClient http;
-        http.begin("http://192.168.178.28:1400/MediaRenderer/AVTransport/Control");
-        http.addHeader("Content-Type", "text/xml");
-        http.addHeader("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue\"");
-        int httpCode = http.POST(query);
-        if (httpCode > 0)
-        {
-            String payload = http.getString();
-            Serial.println(httpCode);
-        }
-        else
-        {
-            Serial.println("ERROR!");
-        }
-    }
+    addtoQueue();
+    play();
     delay(5000);
 }
 
 void addtoQueue(){
     HTTPClient http;
-        http.begin("http://192.168.178.28:1400/MediaRenderer/AVTransport/Control");
-        http.addHeader("Content-Type", "text/xml");
-        http.addHeader("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue\"");
-        int httpCode = http.POST(addtoQueue_Body);
-        if (httpCode > 0)
-        {
-            String payload = http.getString();
-            Serial.println(httpCode);
-        }
-        else
-        {
-            Serial.println("ERROR!");
-        }
+    http.begin("http://192.168.178.28:1400/MediaRenderer/AVTransport/Control");
+    http.addHeader("Content-Type", "text/xml");
+    http.addHeader("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue\"");
+    int httpCode = http.POST(addtoQueue_Body);
+    if (httpCode > 0)
+    {
+        String payload = http.getString();
+        Serial.println(httpCode);
+    }
+    else
+    {
+        Serial.println("ERROR couldn't add to queue!");
+    }
+}
+
+void play(){
+    HTTPClient http;
+    http.begin("http://192.168.178.28:1400/MediaRenderer/AVTransport/Control");
+    http.addHeader("Content-Type", "text/xml");
+    http.addHeader("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#Play\"");
+    int httpCode = http.POST(Play_Body);
+    if (httpCode > 0)
+    {
+        String payload = http.getString();
+        Serial.println(httpCode);
+    }
+    else
+    {
+        Serial.println("ERROR couldn't play!");
+    }
 }
