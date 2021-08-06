adfafc0c6ec6df8230ff73376a42a6e760f6f8b9
Johannes <johannes-pahle@t-online.de>
2021-08-06 11:05:29 +0200

    add song to queue

---
+++ test.py
@@ -0,0 +1,36 @@
+""" This file send a play command to a Sonos device """
+
+import requests
+
+IP = '192.168.178.33'
+ACTION = '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"'
+ENDPOINT = '/MediaRenderer/AVTransport/Control'
+BODY = '''
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+ s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <s:Body>
+    <u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">
+      <InstanceID>0</InstanceID>
+      <EnqueuedURI>https://ia902508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3</EnqueuedURI>
+      <EnqueuedURIMetaData></EnqueuedURIMetaData>
+      <DesiredFirstTrackNumberEnqueued></DesiredFirstTrackNumberEnqueued>
+      <EnqueueAsNext>true</EnqueueAsNext>
+      <FirstTrackNumberEnqueued></FirstTrackNumberEnqueued>
+      <NumTracksAdded></NumTracksAdded>
+      <NewQueueLength></NewQueueLength>
+    </u:AddURIToQueue>
+  </s:Body>
+</s:Envelope>
+'''
+
+HEADERS = {
+    'Content-Type': 'text/xml',
+    'SOAPACTION': ACTION
+}
+
+URL = 'http://{ip}:1400{endpoint}'.format(ip=IP, endpoint=ENDPOINT)
+
+REQ = requests.post(URL, data=BODY, headers=HEADERS)
+
+print("Response:")
+print(REQ.content)
